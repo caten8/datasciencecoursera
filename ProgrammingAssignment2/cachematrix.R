36ad67d88aa208b1e16770d1e32dd77ec34c0093
caten8 <durbinmail@gmail.com>
2015-06-21 12:38:34 -0500

    Minor adjustment to code in cachematrix

--- ProgrammingAssignment2/cachematrix.R
+++ ProgrammingAssignment2/cachematrix.R
@@ -3,15 +3,16 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
-        set <- function(y)
-        {
+        set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
         get <- function() x
         setinverse <- function(inverse) m <<- inverse
         getinverse <- function() m
-        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+        list(set = set, get = get, 
+             setinverse = setinverse, 
+             getinverse = getinverse)
 }
 
 ## The `cacheSolve` function computes the inverse of the special "matrix" returned by 
@@ -19,7 +20,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then `cacheSolve` retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         m <- x$getinverse()
         if(!is.null(m)) {
                 message("getting cached data")
